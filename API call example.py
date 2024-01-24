cbbed4f3714f64bdf471fbaa4e987bdec4672f22
megasloupe <139587361+megasloupe@users.noreply.github.com>
2024-01-24 08:21:00 +0200

    update

--- API call example.py
+++ API call example.py
@@ -1,15 +1,21 @@
 import requests
 
+#Here is how you access the API with the json
+
+#This is the url for the request
 url = 'http://127.0.0.1:5000/calculateDeliveryFee'
+
+#This is the json
 data = {
     'cartValue': 120,
     'deliveryDistance': 3400,
     'numberOfItems': 15
 }
 
+#Here is the request and where you would handle the response
 response = requests.post(url, json=data)
 if response.status_code == 200:
     result = response.json()
-    print(f'Delivery Fee: {result["deliveryFee"]}')
+    print(f'Delivery fee: {result["deliveryFee"]}')
 else:
     print(f'Error: {response.text}')
--- Wolt delivery fee calculator API.py
+++ Wolt delivery fee calculator API.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+#Main calculation function
 def calculateDeliveryFee(cartValue, deliveryDistance, numberOfItems):
     deliveryFee = 0
     if (cartValue >= 200):
@@ -20,19 +21,24 @@ def calculateDeliveryFee(cartValue, deliveryDistance, numberOfItems):
         return 15
     return deliveryFee
 
+#This here takes the json sent by the frontends as shown in the "import requests.py" file
 @app.route('/calculateDeliveryFee', methods=['POST'])
 def CalculatorAPI():
     try:
         data = request.get_json()
         
+        #Makes Python values out of the json
         cartValue = data.get('cartValue', 0)
         deliveryDistance = data.get('deliveryDistance', 0)
         numberOfItems = data.get('numberOfItems', 0)
 
+        #Calls the calculator function
         deliveryFee = calculateDeliveryFee(cartValue, deliveryDistance, numberOfItems)
 
+        #Sends result back as json
         response = {'deliveryFee': deliveryFee}
         return jsonify(response), 200
+    #In case of error sends back the error message
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
